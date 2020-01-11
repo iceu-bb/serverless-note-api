2fe88728556790c9a3bcdae0b2d556c01a68637e
iceu-bb <adamlaskowski737@gmail.com>
2020-01-11 18:24:15 +0100

    Add a Create Note API

---
+++ libs/dynamodb-lib.js
@@ -0,0 +1,9 @@
+import AWS from 'aws-sdk';
+
+export function call(action, params) {
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  return dynamoDb[action](params).promise();
+}
+
+// Promises are a method for managing asynchronous code that serve as an alternative to the standard callback function syntax. It will make our code a lot easier to read.
